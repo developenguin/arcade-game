9b20c621b4455d2f707864b89a58b7335e98fe7f
matthijs <matthijs@heavymetalpenguin.com>
2018-02-25 14:15:49 +0100

    strip whitespace

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
 /* Enemies the player must avoid */
 class Enemy {
-  
+
   constructor(x, y) {
     this.sprite = 'images/enemy-bug.png';
     this.x = x;
     this.y = y;
   }
-  
+
   /* Update the position of the object on the screen, required for game
    * Param: dt, a delta between ticks ensuring equal speed on each computer.
    */
   update(dt) {
-  
+
   }
-  
+
   /* Draw the enemy on the screen */
   render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
@@ -21,62 +21,62 @@ class Enemy {
 }
 
 class Player {
-  
+
   constructor(x, y) {
     this.sprite = 'images/char-boy.png';
     this.x = x;
     this.y = y;
   }
-  
+
   /* Update the position of the player */
   update() {
-  
+
   }
-  
+
   /* Draw the player on screen */
   render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   }
-  
+
   handleInput(direction) {
-  
+
     switch (direction) {
-      
+
       case 'left':
       case 'right':
         this.handleHorizontalInput(direction);
         break;
-        
+
       case 'up':
       case 'down':
         this.handleVerticalInput(direction);
         break;
-        
+
       default:
         break;
-        
+
     }
-    
+
   }
-  
+
   handleHorizontalInput(direction) {
-  
+
     if (direction === 'left') {
       this.x = this.x - 101;
     } else {
       this.x = this.x + 101;
     }
-    
+
   }
-  
+
   handleVerticalInput(direction) {
-  
+
     if (direction === 'up') {
       this.y = this.y - 64;
     } else {
       this.y = this.y + 64;
     }
-    
+
   }
 }
 
