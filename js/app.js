9b371665af3d67ecf468468ae0e73e94ac70b7ac
matthijs <matthijs@heavymetalpenguin.com>
2018-02-25 14:13:46 +0100

    Make dummy player and enemy classes

--- js/app.js
+++ js/app.js
@@ -1,33 +1,92 @@
-// Enemies our player must avoid
-const Enemy = function() {
-  // Variables applied to each of our instances go here,
-  // we've provided one for you to get started
+/* Enemies the player must avoid */
+class Enemy {
+  
+  constructor(x, y) {
+    this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+  }
+  
+  /* Update the position of the object on the screen, required for game
+   * Param: dt, a delta between ticks ensuring equal speed on each computer.
+   */
+  update(dt) {
+  
+  }
+  
+  /* Draw the enemy on the screen */
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+}
 
-  // The image/sprite for our enemies, this uses
-  // a helper we've provided to easily load images
-  this.sprite = 'images/enemy-bug.png';
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-  // You should multiply any movement by the dt parameter
-  // which will ensure the game runs at the same speed for
-  // all computers.
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+class Player {
+  
+  constructor(x, y) {
+    this.sprite = 'images/char-boy.png';
+    this.x = x;
+    this.y = y;
+  }
+  
+  /* Update the position of the player */
+  update() {
+  
+  }
+  
+  /* Draw the player on screen */
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+  
+  handleInput(direction) {
+  
+    switch (direction) {
+      
+      case 'left':
+      case 'right':
+        this.handleHorizontalInput(direction);
+        break;
+        
+      case 'up':
+      case 'down':
+        this.handleVerticalInput(direction);
+        break;
+        
+      default:
+        break;
+        
+    }
+    
+  }
+  
+  handleHorizontalInput(direction) {
+  
+    if (direction === 'left') {
+      this.x = this.x - 101;
+    } else {
+      this.x = this.x + 101;
+    }
+    
+  }
+  
+  handleVerticalInput(direction) {
+  
+    if (direction === 'up') {
+      this.y = this.y - 64;
+    } else {
+      this.y = this.y + 64;
+    }
+    
+  }
+}
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+/* Instantiate objects to use in the game */
+const player = new Player(),
+      allEnemies = [];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
