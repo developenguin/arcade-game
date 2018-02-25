e1a9637dc9e353e11dd8693537d9ea378a1a4305
matthijs <matthijs@heavymetalpenguin.com>
2018-02-25 14:51:12 +0100

    Place some enemies on the screen using row,col notation instead of x,y

--- js/app.js
+++ js/app.js
@@ -1,10 +1,18 @@
 /* Enemies the player must avoid */
 class Enemy {
 
-  constructor(x, y) {
+  /* Initialize the Enemy object.
+   *
+   * Param: row, the row you want the Enemy to appear on
+   * Param: col, the column you want the Enemy to appear on
+   *
+   * Row and Col values are automatically converted to X and Y. Counting starts at 1 (NOT zero)
+   *
+   */
+  constructor(row, col) {
     this.sprite = 'images/enemy-bug.png';
-    this.x = x;
-    this.y = y;
+    this.x = (col - 1) * 101; // 101 is the row width
+    this.y = (row - 1) * 83 - 24; // 83 is row height, 24 is one-time offset from canvas top
   }
 
   /* Update the position of the object on the screen, required for game
@@ -72,9 +80,9 @@ class Player {
   handleVerticalInput(direction) {
 
     if (direction === 'up') {
-      this.y = this.y - 64;
+      this.y = this.y - 83;
     } else {
-      this.y = this.y + 64;
+      this.y = this.y + 83;
     }
 
   }
@@ -86,7 +94,12 @@ class Player {
 
 /* Instantiate objects to use in the game */
 const player = new Player(),
-      allEnemies = [];
+      allEnemies = [
+        new Enemy(2, 1),
+        new Enemy(3, 3),
+        new Enemy(4, 4),
+        new Enemy(4, 5)
+      ];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
