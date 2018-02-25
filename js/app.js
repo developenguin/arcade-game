3850c46a8e21a1453261d481e2ebab9434c860dd
matthijs <matthijs@heavymetalpenguin.com>
2018-02-25 16:40:13 +0100

    listen to key DOWN event instead of UP

--- js/app.js
+++ js/app.js
@@ -191,7 +191,7 @@ const player = new Player(6, 3),
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keydown', function(e) {
   const allowedKeys = {
     37: 'left',
     65: 'left',
