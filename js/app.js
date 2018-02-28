985902a3040b0a1875a58a21a17681398e174a1b
matthijs <matthijs@heavymetalpenguin.com>
2018-02-28 21:38:30 +0100

    Let's leave the file splitting for now

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
-import CONSTANTS from './constants';
+// constants to be used throughout the game
+const CONSTANTS = {
+  dx: 101, // block width
+  dy: 83, // block height
+  offset: 24, // distance from canvas top to the first row,
+  playerCollisionOffset: 24, // horizontal distance from edge of player sprite to first colored pixel
 
+  /*
+   * Points you need to have before bugs move 1 block/second faster
+   */
+  difficultyModifier: 25,
+
+  canvasWidth: 505,
+  canvasHeight: 606
+};
 /* Enemies the player must avoid */
 class Enemy {
 
